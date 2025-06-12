afaca5e7ce659507ad6ceb69ed54de158f71a47e
andresDS24 <andresdiazsolano24@gmail.com>
2025-06-11 20:14:20 -0500

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-# Librerías necesarias
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-from sqlalchemy import create_engine, text
-from datetime import datetime
-
-# (continúa el contenido ya confirmado del documento...)
